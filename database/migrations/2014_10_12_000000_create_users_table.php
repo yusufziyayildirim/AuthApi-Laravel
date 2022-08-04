c4e69b10dc421a1ba0890160dbc9de6c2878eea3
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-08-04 16:56:16 +0300

    Migrations created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('email_verified')->default(0)->change();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
