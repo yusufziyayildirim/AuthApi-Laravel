2e42f78eea9fcbcc72bb56b7892dcbcc3cdbbeb2
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-08-04 17:00:04 +0300

    Response.php created

---
+++ app/Http/Controllers/Response.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Response
+{
+    private $status, $message, $data;
+
+    public static function withData($status, $message, $data)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+
+    public static function withoutData($status, $message)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => []
+        ]);
+    }
+}
